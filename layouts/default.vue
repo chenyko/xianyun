593ac5e73f39ec498231e620657e22699c7d7eb6
chenyko <55200520+chenyko@users.noreply.github.com>
2019-11-09 16:06:04 +0800

    第一次提交和首页轮播图

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Header/>
+    <nuxt />
+    <Footer/>
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang='less'>
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,li,ol{
+  list-style: none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+}
+em,i{
+  font-style: normal;
+}
+
+</style>
