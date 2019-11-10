61d547b56e263100c661679278892943fb34eb27
chenyko <55200520+chenyko@users.noreply.github.com>
2019-11-10 14:34:25 +0800

    注册表单验证

---
+++ components/user/registerForm.vue
@@ -0,0 +1,139 @@
+<template>
+    <el-form 
+        :model="form" 
+        ref="form" 
+        :rules="rules" 
+        class="form">
+            <el-form-item class="form-item" prop="username">
+                <el-input 
+                placeholder="用户名/手机号"
+                v-model="form.username">
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="captcha">
+                <el-input 
+                placeholder="验证码" 
+                v-model="form.captcha">
+                    <template slot="append">
+                        <el-button >
+                        <!-- @click="handleSendCaptcha" -->
+                        
+                            发送验证码
+                        </el-button>
+                    </template>
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop='nickname'>
+                <el-input 
+                placeholder="你的名字"
+                v-modle="form.nickname">
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="password">
+                <el-input 
+                placeholder="密码" 
+                type="password"
+                v-model="form.password"></el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="checkPassword">
+                <el-input 
+                placeholder="确认密码" 
+                type="password"
+                v-model="form.checkPassword">
+                </el-input>
+            </el-form-item>
+
+            <el-button 
+            class="submit" 
+            type="primary" 
+           >
+            <!-- @click="handleRegSubmit" -->
+                注册
+            </el-button>
+        </el-form>
+</template>
+<script>
+export default {
+   data(){
+       return{
+        form:{
+            username:'',
+            nickname:'',
+            password:'',
+            captcha:'',
+            checkPassword:'',
+        },
+        rules:{
+             username:[
+              {
+              required:true,
+              message:'请输入用户名',
+              trigge:'blur'
+              }
+          ],
+          password:[
+              {
+              required:true,
+               message: '请输入密码',
+              trigge:'blur'
+              }
+          ],
+           nickname:[
+              {
+              required:true,
+              message:'请输入昵称',
+              trigge:'blur'
+              }
+          ],
+          captcha:[
+              {
+              required:true,
+               message: '请输入验证码',
+              trigge:'blur'
+              }
+          ],
+           checkPassword:[
+              {
+              required:true,
+              message: '请确认密码',
+              trigge:'blur'
+              }
+          ]
+        }
+       }
+   },
+   methods:{
+       handleSendCaptcha(){
+
+       },
+      handleSendCaptcha(){
+
+      }
+   }
+}
+</script>
+<style lang="less" scoped>
+  .form{
+        padding:25px;
+    }
+
+    .form-item{
+        margin-bottom:20px;
+    }
+
+    .form-text{
+        font-size:12px;
+        color:#409EFF;
+        text-align: right;
+        line-height: 1;
+    }
+
+    .submit{
+        width:100%;
+        margin-top:10px;
+    }
+</style>
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -14,7 +14,7 @@
         <LoginForm v-if="currentTab == 0"/>
 
         <!-- 注册功能组件 -->
-        <!-- <RegisterForm v-if="currentTab == 1"/> -->
+        <RegisterForm v-if="currentTab == 1"/>
       </div>
     </el-row>
   </div>
@@ -22,9 +22,11 @@
 
 <script>
 import LoginForm from "@/components/user/loginForm"
+import RegisterForm from "@/components/user/registerForm"
 export default {
   components:{
-  LoginForm
+  LoginForm,
+  RegisterForm
   },
     data(){
         return{
