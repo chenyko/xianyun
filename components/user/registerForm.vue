7391e077c2ad81dea450b13373220ddaee600353
chenyko <55200520+chenyko@users.noreply.github.com>
2019-11-10 16:06:29 +0800

    发送验证码验证完成

--- components/user/registerForm.vue
+++ components/user/registerForm.vue
@@ -1,150 +1,153 @@
 <template>
-    <el-form 
-        :model="form"
-        ref="form" 
-        :rules="rules" 
-        class="form">
-            <el-form-item class="form-item" prop="username">
-                <el-input 
-                placeholder="用户名/手机号"
-                v-model="form.username">
-                </el-input>
-            </el-form-item>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名/手机号" v-model="form.username"></el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item" prop="captcha">
-                <el-input 
-                placeholder="验证码" 
-                v-model="form.captcha">
-                    <template slot="append">
-                        <el-button >
-                        <!-- @click="handleSendCaptcha" -->
-                        
-                            发送验证码
-                        </el-button>
-                    </template>
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="captcha">
+      <el-input placeholder="验证码" v-model="form.captcha">
+        <template slot="append">
+          <el-button @click="handleSendCaptcha('ruleForm')">发送验证码</el-button>
+        </template>
+      </el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item" prop='nickname'>
-                <el-input 
-                placeholder="你的名字"
-                v-model="form.nickname">
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="nickname">
+      <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item" prop="password">
-                <el-input 
-                placeholder="密码" 
-                type="password"
-                v-model="form.password"></el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item" prop="checkPassword">
-                <el-input 
-                placeholder="确认密码" 
-                type="password"
-                v-model="form.checkPassword">
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="checkPassword">
+      <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"></el-input>
+    </el-form-item>
 
-            <el-button 
-            class="submit" 
-            type="primary" 
-           >
-            <!-- @click="handleRegSubmit" -->
-                注册
-            </el-button>
-        </el-form>
+    <el-button class="submit" type="primary">
+      <!-- @click="handleRegSubmit" -->
+      注册
+    </el-button>
+  </el-form>
 </template>
 <script>
 export default {
-   data(){
-
+  data() {
     //   确认密码
-    const validatePass=(rules,value,callback)=>{
-       if(value===''){
-          callback(new Error('请再次输入密码'))
-       }else if(value !=this.password){
-           callback(new Error('两次输入密码不一致'))
-       }else{
-           callback()
-       }
-    }
-       return{
-        form:{
-            username:'',
-            nickname:'',
-            password:'',
-            captcha:'',
-            checkPassword:'',
-        },
-        rules:{
-             username:[
-              {
-              required:true,
-              message:'请输入用户名',
-              trigge:'blur'
-              }
-          ],
-          password:[
-              {
-              required:true,
-               message: '请输入密码',
-              trigge:'blur'
-              }
-          ],
-           nickname:[
-              {
-              required:true,
-              message:'请输入昵称',
-              trigge:'blur'
-              }
-          ],
-          captcha:[
-              {
-              required:true,
-               message: '请输入验证码',
-              trigge:'blur'
-              }
-          ],
-           checkPassword:[
-              {
-              validator: validatePass, 
-           
-              trigge:'blur'
-              }
-          ]
-        }
-       }
-   },
-   methods:{
-       handleSendCaptcha(){
-
-       },
-      handleSendCaptcha(){
-
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value != this.password) {
+        callback(new Error("两次输入密码不一致"));
+      } else {
+        callback();
       }
-   }
-}
+    };
+    return {
+      form: {
+        username: "",
+        nickname: "",
+        password: "",
+        captcha: "",
+        checkPassword: ""
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigge: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigge: "blur"
+          }
+        ],
+        nickname: [
+          {
+            required: true,
+            message: "请输入昵称",
+            trigge: "blur"
+          }
+        ],
+        captcha: [{ 
+                required: true, 
+                message: '请输入验证码', 
+                trigger: 'blur' 
+            }],
+        checkPassword: [
+          {
+            validator: validatePass,
+           
+            trigge: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleSendCaptcha() {
+      console.log("点击了发送验证码按钮");
+
+      if(!this.form.username){
+        this.$confirm('手机号码不能为空', '提示', {
+            confirmButtonText: '确定',
+            showCancelButton: false,
+            type: 'warning'
+        })
+        return;
+    }
+
+    if(this.form.username.length !== 11){
+        this.$confirm('手机号码格式错误', '提示', {
+            confirmButtonText: '确定',
+            showCancelButton: false,
+            type: 'warning'
+        })
+        return;
+    }
+
+
+    this.$axios({
+        url: `/captchas`,
+        method: "POST",
+        data: {
+            tel: this.form.username
+        }
+    }).then(res => {
+        const {code} = res.data;
+        this.$confirm(`模拟手机验证码为：${code}`, '提示', {
+            confirmButtonText: '确定',
+            showCancelButton: false,
+            type: 'warning'
+        })
+    })
+    },
+    
+  }
+};
 </script>
 <style lang="less" scoped>
-  .form{
-        padding:25px;
-    }
+.form {
+  padding: 25px;
+}
 
-    .form-item{
-        margin-bottom:20px;
-    }
+.form-item {
+  margin-bottom: 20px;
+}
 
-    .form-text{
-        font-size:12px;
-        color:#409EFF;
-        text-align: right;
-        line-height: 1;
-    }
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
 
-    .submit{
-        width:100%;
-        margin-top:10px;
-    }
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
 </style>
