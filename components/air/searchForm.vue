b0bf89c445bddd17ba9192ec350969dc3d01e572
chenyko <55200520+chenyko@users.noreply.github.com>
2019-11-12 20:25:56 +0800

    根绝搜索建议返回城市列表

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -15,6 +15,7 @@
                 <!-- fetch-suggestions 返回输入建议的方法 -->
                 <!-- select 点击选中建议项时触发 -->
                 <el-autocomplete
+                v-model="form.departCity"
                 :fetch-suggestions="queryDepartSearch"
                 placeholder="请搜索出发城市"
                 @select="handleDepartSelect"
@@ -23,6 +24,7 @@
             </el-form-item>
             <el-form-item label="到达城市">
                 <el-autocomplete
+                v-model="form.destCity"
                 :fetch-suggestions="queryDestSearch"
                 placeholder="请搜索到达城市"
                 @select="handleDestSelect"
@@ -32,6 +34,7 @@
             <el-form-item label="出发时间">
                 <!-- change 用户确认选择日期时触发 -->
                 <el-date-picker type="date" 
+                v-model="form.departDate"
                 placeholder="请选择日期" 
                 style="width: 100%;"
                 @change="handleDate">
@@ -60,7 +63,19 @@ export default {
                 {icon: "iconfont icondancheng", name: "单程"},
                 {icon: "iconfont iconshuangxiang", name: "往返"}
             ],
+            form:{
+                departCity:'',    // 出发城市
+                departCodde:'',   // 出发城市代码
+                destCity:'',   //到达城市
+                destCode:'',    //到达城市代码
+                departDate:'',  //出发日期
+            },
             currentTab: 0,
+
+            // 出发城市列表
+            departCities:[],
+            // 到达城市列表
+            destCities:[],
         }
     },
     methods: {
@@ -72,21 +87,46 @@ export default {
         // 出发城市输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
         queryDepartSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
+            if(!value.trim()){
+                return cb([])
+            }
+            this.$axios({
+                url:'/airs/city',
+                params:{
+                    name:value
+                }
+            }).then(res=>{
+                console.log(res.data);
+                const {data}=res.data;
+               this.departCities=data.map(v=>{
+                    v.value=v.name.replace("市", "")
+                    return v
+                })
+                cb(this.departCities)
+            })
+            
         },
 
         // 目标城市输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
         queryDestSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
+            if(!value.trim()){
+                return cb([])
+            }
+            this.$axios({
+                url:'/airs/city',
+                params:{
+                    name:value
+                }
+            }).then(res=>{
+                console.log(res.data);
+                const {data}=res.data;
+               this.destCities=data.map(v=>{
+                    v.value=v.name.replace("市", "")
+                    return v
+                })
+                cb(this.destCities)
+            })
         },
        
         // 出发城市下拉选择时触发
